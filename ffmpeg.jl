677941bb01051f5017a9982b4d21849ae9963994
neomatrix <josuecevedo@gmail.com>
2015-10-28 11:56:25 -0600

    update

--- ffmpeg.jl
+++ ffmpeg.jl
@@ -1,4 +1,3 @@
-
 type Cam
 	nombre::String
 	usuario::String
@@ -12,20 +11,20 @@ type Cam
 end
 
 function optvideo(x::Cam)
-	run(`ffmpeg -re -i http://$(x.usuario):$(x.contra)@$(x.ip)/video.cgi -ss 00:00:00 -t $(x.duracion) -c copy -y $(x.ubicacionv)`)
+	run(`ffmpeg -re -i http://$(x.usuario):$(x.contra)@$(x.ip)/video/mjpg.cgi -ss 00:00:00 -t $(x.duracion) -c copy -y $(x.ubicacionv)`)
 	#=
 	-------------------------------------------------------------video---------------------------------
-	ffmpeg -re -i http://admin:@192.168.1.10/video.cgi -ss 00:00:00 -t 00:00:10 -c copy -y /home/neomatrix/Escritorio/vi.avi
-	<img style="-webkit-user-select: none" src="http://192.168.1.10/video.cgi">
+	ffmpeg -re -i http://admin:@192.168.1.10/video/mjpg.cgi -ss 00:00:00 -t 00:00:10 -c copy -y /home/neomatrix/Escritorio/vi.avi
+	<img style="-webkit-user-select: none" src="http://192.168.1.10/video/mjpg.cgi">
 	=#
-	print("ffmpeg -re -i http://$(x.usuario):$(x.contra)@$(x.ip)/video.cgi -ss 00:00:00 -t $(x.duracion) -c copy -y $(x.ubicacionv)")
+	print("ffmpeg -re -i http://$(x.usuario):$(x.contra)@$(x.ip)/video/mjpg.cgi -ss 00:00:00 -t $(x.duracion) -c copy -y $(x.ubicacionv)")
 end
 
 function optimg(x::Cam)
-	run(`ffmpeg -i http://$(x.usuario):$(x.contrase)@$(x.ip)/video.cgi -frames:v 1 -ss 00:00:00 -f image2 $(x.ubicacioni)`)
+	run(`ffmpeg -i http://$(x.usuario):$(x.contrase)@$(x.ip)/video/mjpg.cgi -frames:v 1 -ss 00:00:00 -f image2 $(x.ubicacioni)`)
 	#=	
 	--------------------------------------------------------------imagen---------------------------
-	ffmpeg -i http://admin:@192.168.1.10/video.cgi -frames:v 1 -ss 00:00:00 -f image2 /home/neomatrix/Escritorio/imagen2.jpg
+	ffmpeg -i http://admin:@192.168.1.10/video/mjpg.cgi -frames:v 1 -ss 00:00:00 -f image2 /home/neomatrix/Escritorio/imagen2.jpg
 																							--- experimental
 	ffmpeg -i http://admin:@192.168.1.10/image/jpeg.cgi /home/neomatrix/Escritorio/imagen2.jpg
 
